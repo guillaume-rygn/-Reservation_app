31c7d2f328db618d5d8fba03d6dac7e7194fd624
guillaume <reygner0224@gmail.com>
2022-09-18 21:47:26 +0200

    final render for project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -156,11 +156,9 @@ export default {
       }
     },
     deletereservation(id) {
-      console.log(id)
       axios
         .delete(`https://adlin-rest-api.herokuapp.com/api/v1/reservations/${id}`)
         .then(() => {
-          console.log("ici")
           const mynewreservation = localStorage
             .getItem("myreservation")
             .split(",")
